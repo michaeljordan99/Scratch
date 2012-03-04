9fd201b9656c100c6dba14a65ace92c6f15e8b90
Michael Jordan <michaeljordan99@hotmail.com>
2012-03-04 08:17:33 -0800

    Added Fluent Interface Stuff - w/Add

---
+++ Scratch.Tests/FluentInterfaceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scratch.Models;
+
+namespace Scratch.Tests
+{
+    [TestClass]
+    public class FluentInterfaceTests
+    {
+        [TestMethod]
+        public void Fluent_Interface_Add_Member_To_Roles_Test()
+        {
+            IMember member = new Member("Michael", "Jordan");
+            member.AddToRole("User").AddToRole("Reader").AddToRole("Writer").Create();
+            Assert.IsTrue(member.Roles.Count == 3);
+        }
+    }
+}
---
+++ Scratch/Models/Member.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Scratch.Models
+{
+    /*Fluent Interface Example*/
+    public interface IMember
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        List<string> Roles { get; set; }
+        IMember AddToRole(string role);
+        IMember Create();
+    }
+
+    public class Member : IMember
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> Roles { get; set; }
+
+        public Member()
+        {
+            this.Roles = new List<string>();
+        }
+
+        public Member(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Roles = new List<string>();
+        }
+
+        public IMember AddToRole(string role)
+        {
+            this.Roles.Add(role);
+            return this;
+        }
+
+        public IMember Create()
+        {
+            foreach (var role in this.Roles)
+            {
+
+            }
+
+            return this;
+        }
+    }
+}
